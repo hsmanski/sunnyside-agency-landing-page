17d31ad33fd2513f80ca216b67cda8872aab0fda
Heather <64887470+hsmanski@users.noreply.github.com>
2021-11-16 20:19:07 -0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+});
+
+document
+  .querySelector(".modal-link-container")
+  .addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+  });
